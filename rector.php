7234e1dceb6fe9df072c458d7cb8739945ab2dc0
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2025-05-07 01:36:25 +0300

    Update packages, add Rector

---
+++ rector.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\StringableForToStringRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withParallel()
+    ->withCache(__DIR__ . '/var/rector')
+    ->withPhpSets()
+    ->withSkip([
+        StringableForToStringRector::class,
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+    ]);
